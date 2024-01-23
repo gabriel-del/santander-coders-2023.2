3bce6722da3f216562b05feda72c383b155e3381
Gabriel Del <gabrieldelc.barros@gmail.com>
2024-01-23 15:29:24 -0300

    add all files from module 1

---
+++ m1/random.py
@@ -0,0 +1,20 @@
+
+import randrange
+
+# print(randrange(-5, 5))
+
+# import math
+
+# let alunos = [...Array(29).keys()]
+# let grupos = [
+#   ["nome1"],
+#   ["nome2"],
+#   ["nome3"], 
+#   ["nome4"],
+#   ["nome5"],
+#   ["nome6"]
+# ]
+
+# console.log(random(1,30))
+
+# console.log(grupos.length)
