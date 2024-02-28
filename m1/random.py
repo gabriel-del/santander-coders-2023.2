7feea3ecc2d8cf5006005ce7a7cf952c40006c66
Gabriel Del <gabrieldelc.barros@gmail.com>
2024-02-28 15:39:24 -0300

    finish module 2

--- m1/random.py
+++ m1/random.py
@@ -1,27 +1,6 @@
 
-import randrange
+import random2
 
-# print(randrange(-5, 5))
-
-# import math
-
-# let alunos = [...Array(29).keys()]
-# let grupos = [
-#   ["nome1"],
-#   ["nome2"],
-#   ["nome3"],
-#   ["nome4"],
-#   ["nome5"],
-#   ["nome6"]
-# ]
-
-# console.log(random(1,30))
-
-# console.log(grupos.length)
-
-
-# import random as rd
-
-# n = 20
-# rand_num = [rd.randint(1,100) for _ in range(n)]
-# print(rand_num)
+a = [random2.randrange(0,10) for _ in range(20)]
+# print(random2)
+print(a)
